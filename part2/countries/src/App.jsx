1298cf618e820a6960dc20c11b3f9049345f2a22
MrLrctus <lilbrown3000@gmail.com>
2023-04-14 18:56:28 +0100

    from termux

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -3,24 +3,38 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 const App = () => {
-  const [country, setCountry] = useState(null);
+  const [country, setCountry] = useState([]);
+  const [query, setQuery] = useState('');
+  const [message, setMessage] = useState('');
   const fetchCountry = async () => {
     try {
-      const request = await axios.get('https://restcountries.com/all');
+      const request = await axios.get(`https://restcountries.com/v3.1/all?fields=name`);
       const data = await request.data;
-      console.log('render..');
       setCountry(data);
     } catch (err) {
       console.log('cannot fetch, timeout');
     }
   };
-  useEffect(() => async () => fetchCountry());
-
+  useEffect(() => async () => fetchCountry(), []);
+  const handleFilter = (event) => {
+    event.preventDefaults();
+    alert(query);
+    const temp = country?.slice();
+    const filtered = temp.filter((name) => name.name.common.toLowerCase().match(query.toLowerCase()));
+    if (filtered.length > 10) {
+      setMessage("too many matches, specify another filter");
+    } else {
+      setCountry(filtered);
+    }
+  }
   return (
     <div className="mt-4 mx-4">
-      <form>
-        find countries <input className="outline-none rounded-md border p-2" />
+      <form onSubmit={(event) => handleFilter(event)}>
+        find countries <input className="outline-none rounded-md border p-2" onChange={() => setQuery(event.target.value)} value={query} />
       </form>
+      {message? country.map((name) => {
+        return <p>{name.name.common}</p>
+      }) : <p>{message}</p>}
     </div>
   );
 };
