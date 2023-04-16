c20d3db4f99278c4ad14fbbbfc553d16150e8571
BrownCarter <lilbrown3000@gmail.com>
2023-04-16 02:30:38 +0100

    complete

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,21 +1,23 @@
 import axios from 'axios';
+import { useCallback } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
 const App = () => {
   const [country, setCountry] = useState([]);
   const [filtered, setFiltered] = useState([]);
+  const [weather, setWeather] = useState([]);
   const [query, setQuery] = useState('');
   const [message, setMessage] = useState(null);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
   const fetchCountry = async () => {
     try {
-      const request = await axios.get(
-        `https://restcountries.com/v3.1/all?fields=name`
-      );
-      const data = await request.data;
+      const request = await axios.get('https://restcountries.com/v2/all');
+      const data = request.data;
       setCountry(data);
     } catch (err) {
-      console.log('cannot fetch, timeout');
+      console.error('cannot fetch country');
     }
   };
   useEffect(() => async () => fetchCountry(), []);
@@ -23,7 +25,7 @@ const App = () => {
     event.preventDefault();
     const temp = country?.slice();
     const filtered = temp.filter((name) =>
-      name.name.common.toLowerCase().match(query.toLowerCase())
+      name.name.toLowerCase()?.match(query.toLowerCase())
     );
     if (filtered.length > 10) {
       setMessage('too many matches, specify another filter');
@@ -32,6 +34,84 @@ const App = () => {
       setMessage(null);
     }
   };
+  const fetchWeather = useCallback(async () => {
+    try {
+      let request = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${
+          import.meta.env.VITE_API_KEY
+        }`
+      );
+      let data = request.data;
+      setWeather(data);
+    } catch (err) {
+      console.error('cannot fetch weather');
+    }
+  }, [lat, lng]);
+
+  useEffect(() => {
+    if (filtered.length == 1) {
+      setLat(filtered[0].latlng[0]);
+      setLng(filtered[0].latlng[1]);
+    }
+    fetchWeather();
+  }, [query, lat, lng, filtered, fetchWeather]);
+
+  const filteredMany = !message ? (
+    filtered.map((name) => {
+      return <p key={name.name}>{name.name}</p>;
+    })
+  ) : (
+    <p>{message}</p>
+  );
+  const filteredOne = !message ? (
+    filtered.map((name) => {
+      let {
+        capital,
+        area,
+        languages,
+        flags: { svg },
+      } = name;
+      return (
+        <div className="flex flex-col gap-4">
+          <p className="text-lg font-bold" key={name.name}>
+            {name.name}
+          </p>
+          <p> Capital {capital}</p>
+          <p> Area {area}</p>
+          <div className="">
+            {' '}
+            <b>languages</b>
+            {languages.map((lang) => {
+              return (
+                <ul className="list-disc mx-10" key={lang.name}>
+                  <li>{lang.name}</li>
+                </ul>
+              );
+            })}
+          </div>
+          <div className="w-52 h-24">
+            <img src={svg} alt={name.name} />
+          </div>
+          <div className="mt-7">
+            <b> Weather in {capital} </b>
+            <p>
+              {' '}
+              temperature {(weather.main.temp - 273.15).toFixed(2)} Celcius
+            </p>
+            <div className="w-52 h-24">
+              <img
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={name.name}
+              />
+            </div>
+            <p>Wind {weather.wind.speed} m/s </p>
+          </div>
+        </div>
+      );
+    })
+  ) : (
+    <p>{message}</p>
+  );
   return (
     <div className="mt-4 mx-4">
       <form onSubmit={handleFilter}>
@@ -42,13 +122,7 @@ const App = () => {
           value={query}
         />
       </form>
-      {!message ? (
-        filtered.map((name) => {
-          return <p key={name.name.common}>{name.name.common}</p>;
-        })
-      ) : (
-        <p>{message}</p>
-      )}
+      {filtered.length === 1 ? filteredOne : filteredMany}
     </div>
   );
 };
